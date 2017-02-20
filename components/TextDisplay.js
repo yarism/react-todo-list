a42d5650c2243170252c130d96d9f3872c995060
Joakim Johansson <joakim.y.johansson@teliacompany.com>
2017-02-20 13:08:02 +0100

    something something

--- components/TextDisplay.js
+++ components/TextDisplay.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 class TextDisplay extends Component {
 
-  render() {
-    return <div>I'm displaying text from my parent: {this.props.text}</div>
-  }
+	handleClick() {
+		this.props.deleteLetter()
+	}
+
+	render() {
+		return (
+			<div>
+				<div>I'm displaying text from my parent: {this.props.text}</div>
+				<button onClick={this.handleClick.bind(this)}>delete one letter</button>
+			</div>
+		)
+	}
 
 }
 
